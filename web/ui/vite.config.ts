13c4b8df773de135e881c1709b863f795e8c2840
kemalmus <75433787+kemalmus@users.noreply.github.com>
2025-10-23 18:31:54 +0000

    fixed UI frontend, looks terrible

--- web/ui/vite.config.ts
+++ web/ui/vite.config.ts
@@ -1,26 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: "0.0.0.0",
     port: 5000,
     strictPort: true,
+    allowedHosts: true,
     hmr: {
       clientPort: 443,
     },
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
+      "/api": {
+        target: "http://localhost:8000",
         changeOrigin: true,
       },
     },
   },
   build: {
-    outDir: '../static/ui',
+    outDir: "../static/ui",
     emptyOutDir: true,
   },
-})
-
-
+});
